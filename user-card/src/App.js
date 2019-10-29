d9cc0a9511acc55c45f3b971822485f28110ea93
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-10-29 15:44:36 -0500

    axios add user and friends to state

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import Usercard from './components/usercard/Usercard';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      user: {},
+      friends: []
     }
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/easyas123l1`)
+    .then(results => {
+      this.setState({ user: results.data})
+      return results.data.followers_url
+    })
+    .then(results2 => {
+      return axios.get(results2);
+    })
+    .then(results3 => {
+      results3.data.forEach(friend => {
+        axios.get(friend.url)
+        .then(results4 => {
+          this.setState({ friends: [...this.state.friends, results4.data]})
+        })
+      })
+    }) 
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>My Github:<span role='img' >❤️</span>'s</h1>
+        <Usercard />
       </div>
     );
   }
